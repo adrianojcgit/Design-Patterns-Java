fb39dc2fefe97adc6967cd82a9a547abcb3f1010
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-09-11 10:38:21 -0300

    Initial commit

---
+++ Criacionais/src/factory/method/v1/factory/IPhoneFactory.java
@@ -0,0 +1,20 @@
+package factory.method.v1.factory;
+
+import factory.method.v1.model.IPhone;
+
+public abstract class IPhoneFactory {
+    public IPhone orderIPhone() {
+        IPhone device = null;
+
+        device = createIPhone();
+
+        device.getHardware();
+        device.assemble();
+        device.certificates();
+        device.pack();
+
+        return device;
+    }
+
+    protected abstract IPhone createIPhone();
+}
