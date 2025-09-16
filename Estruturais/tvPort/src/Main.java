8a8b6f2a66e83c0d18af8d31dc83360bb36fa452
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-09-16 17:48:36 -0300

    Padrão Estrutural Adapter baseado em Objetos.

---
+++ Estruturais/tvPort/src/Main.java
@@ -0,0 +1,24 @@
+import adapters.HDMIToVGAAdapter;
+import devices.Computer;
+import devices.OldMonitor;
+import devices.TV;
+
+public class Main {
+    public static void main(String[] args) {
+       Computer pc = new Computer();
+       TV tv = new TV();
+       pc.connectPort(tv);
+       pc.sendImageAndSound("Cat and rainbow", "Nyan cat song");
+
+       System.out.println("------ Monitor ----------");
+
+        Computer pc2 = new Computer();
+        OldMonitor monitor = new OldMonitor();
+        //pc2.connectPort(monitor.getConnector());  //Don't work! That's the reason why we create the adapter
+        pc2.connectPort(new HDMIToVGAAdapter(monitor));
+        pc2.sendImageAndSound("Cat and rainbow", "Nyan cat song");
+
+        System.out.println("------ Monitor Class Adapter ----------");
+        
+    }
+}
---
+++ Estruturais/tvPort/src/devices/OldMonitor.java
@@ -0,0 +1,10 @@
+package devices;
+
+import interfaces.VGA;
+
+public class OldMonitor implements VGA {
+    @Override
+    public void setImage(String image) {
+        System.out.println(">>> This is your video: " + image);
+    }
+}
