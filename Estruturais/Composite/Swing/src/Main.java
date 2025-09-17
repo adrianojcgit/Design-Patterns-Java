8833b6a66b2ad5d22dac4f82998117d828550b91
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-09-17 18:09:14 -0300

    Criando um formulário para exemplificar o Padrão Composite.

--- Estruturais/Composite/Swing/src/Main.java
+++ Estruturais/Composite/Swing/src/Main.java
@@ -1,15 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        JFrame frame =new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        JLabel title = new JLabel("TITLE");
+        frame.add(title, BorderLayout.NORTH);
+
+        JPanel panel = new JPanel(new FlowLayout());
+        JLabel label = new JLabel("Label #1");
+        JButton button= new JButton("Button #1");
+        panel.add(label);
+        panel.add(button);
+
+        frame.add(panel, BorderLayout.CENTER);
+
+        frame.setSize(400,200);
+        frame.setVisible(true);
     }
 }
