39ef50585a8752c1d8599d3fabf4ea193edddb44
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-09-22 17:55:15 -0300

    Exemplo de Padrão Observer com Swing.

---
+++ Comportamentais/Observer/ObserverSwing/src/Main.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        JPanel labelPanel = new JPanel();
+        labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
+        Label labelB1 = new Label("Button 1");
+        Label labelB1B2 = new Label("Button 1/2");
+        Label labelB2 = new Label("Button 2");
+        labelPanel.add(labelB1);
+        labelPanel.add(labelB1B2);
+        labelPanel.add(labelB2);
+
+        frame.add(labelPanel, BorderLayout.CENTER);
+
+        Button b1 = new Button("Button #1", labelB1, labelB1B2);
+        Button b2 = new Button("Button #2", labelB1B2, labelB2);
+
+        frame.add(b1, BorderLayout.WEST);
+        frame.add(b2, BorderLayout.EAST);
+
+        frame.setSize(800, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
