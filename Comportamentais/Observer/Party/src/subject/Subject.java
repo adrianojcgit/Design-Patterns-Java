9ccd278a15632ddc16c90ce307390f3941182d52
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-09-22 17:48:44 -0300

    Exemplo do Padrão Observer.

---
+++ Comportamentais/Observer/Party/src/subject/Subject.java
@@ -0,0 +1,20 @@
+package subject;
+
+import observers.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Subject {
+    private List<Observer> observers = new ArrayList<Observer>();
+
+    public void add(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers(boolean status) {
+        for(Observer o: observers) {
+            o.update(status);
+        }
+    }
+}
